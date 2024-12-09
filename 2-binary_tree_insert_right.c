aca3c538c9570c0795d7db3953c1ce1844dab071
sebgrgs <sebastien.georges11@gmail.com>
2024-12-09 12:21:50 +0100

    add function to delete an entire binary tree and update header file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -38,17 +38,3 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	return (new_node);
 
 }
-int main(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_print(root);
-    printf("\n");
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-    return (0);
-}
