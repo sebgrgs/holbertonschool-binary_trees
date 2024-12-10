ff27c4cbf0498964ed9b1feabeb819aee1416b4d
sebgrgs <sebastien.georges11@gmail.com>
2024-12-10 14:00:57 +0100

    refactor binary_tree_is_full and add binary_tree_balance function for improved tree balance measurement

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,11 +32,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if full, 0 if not full or tree is NULL
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ * Return: The balance factor of the tree starting at @node
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect, 0 if not perfect or tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
@@ -48,35 +69,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 	}
 
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-	}
-
-	return (0);
-}
-
-/**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if perfect, 0 if not perfect or tree is NULL
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height != right_height)
-	{
-		return (0);
-	}
-
-	return (binary_tree_is_full(tree));
+	return(!binary_tree_balance(tree) &&
+		binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
 }
